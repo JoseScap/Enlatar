414142a7e1072358180b36822c7ca068c6006a6d
Juan José Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2025-02-15 16:16:00 -0300

    Add AlertType and AlertProps types for notification styling

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,1 +1,8 @@
 export type Size = "sm" | "md" | "lg" | "xl";
+
+export type AlertType = "info" | "warning" | "success" | "error";
+export type AlertProps = {
+    icon: string,
+    color: string,
+    transparentColor: string,
+}

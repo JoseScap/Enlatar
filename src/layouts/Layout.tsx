36245bfc89b498a8cc6adfb14557bb4458838d50
Juan José Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2025-02-15 04:11:31 -0300

    Implement sidebar toggle functionality in Layout component

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,11 +1,20 @@
 import Header from "@/components/Header/Header"
 import s from "./Layout.module.scss"
+import { Sidebar } from "@/components";
+import { useState } from "react";
 
 const Layout = () => {
+  const [sidebarOpened, setSidebarOpened] = useState(true);
+
+  const toggleSidebar = () => {
+    setSidebarOpened(!sidebarOpened);
+  };
+
   return (
     <div className={s.root}>
       <div className={s.wrap}>
-        <Header />
+        <Header onSidebarToggle={toggleSidebar} />
+        <Sidebar sidebarOpened={sidebarOpened} />
       </div>
     </div>
   );
